947fc0af7ebe7f95b2d4c15bf6ed6d58b375b49f
Matthew Heimark <matthew.heimark@gmail.com>
2019-08-05 08:28:33 -0500

    html, css and images for index page UI

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+
+    // Boilerplate code given in class on first day of project
+    //
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+
+    res.render("index", {});
   });
 
   // Load example page and pass in an example by id
