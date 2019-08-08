9c6e1625538048ae6ba908eecb6d251438f2345a
Andrew P <andrewpetersondev@gmail.com>
2019-08-08 13:53:29 -0500

    added signup handlebars, updated htmlroutes, added links to signup css in main handlebars, etc

--- models/username.js
+++ models/username.js
@@ -1,10 +1,10 @@
-module.exports = function(sequelize, DataTypes){
-    var Username = sequelize.define("Username",{
+module.exports = function (sequelize, DataTypes) {
+    var Username = sequelize.define("Username", {
         name: DataTypes.STRING
         //user will type their username(into the parent table)- to bring up previous saves(url and category in child table)
     });
 
-    Username.associate = function(models){
+    Username.associate = function (models) {
         //username in connected to post.js
         Username.hasMany(models.Post, {
             onDelete: "cascade"
