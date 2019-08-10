7ead6c2913bca604cbdc4e3307314167ad55506d
idiatou <idiatoudiallo1741@gmail.com>
2019-08-10 12:58:55 -0500

    tables

---
+++ models/users.js
@@ -0,0 +1,15 @@
+module.exports = function(sequelize, DataTypes) {
+    var user = sequelize.define("user", {
+        
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    });
+  
+
+
+      
+
+    return user;
+  };
+
