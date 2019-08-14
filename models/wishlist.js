820446dad1406226764d0d5451d8ee7f7c182995
unknown <galan1451@gmail.com>
2019-08-13 21:30:02 -0500

    multiple columns in database

--- models/wishlist.js
+++ models/wishlist.js
@@ -1,8 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
   var WishList = sequelize.define("WishList", {
+    store_id: DataTypes.TEXT,
     name: DataTypes.STRING,
+    color: DataTypes.TEXT,
+    size: DataTypes.TEXT,
+    rating: DataTypes.TEXT,
     description: DataTypes.TEXT
+  },{
+    freezeTableName: true
   });
-  
   return WishList;
 };          
