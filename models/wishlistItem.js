604e39cdd666febe2c2d469a8d7ef21ac7ad6eb8
idiatou <idiatoudiallo1741@gmail.com>
2019-08-15 18:40:53 -0500

    seeds

--- models/wishlistItem.js
+++ models/wishlistItem.js
@@ -30,5 +30,5 @@ module.exports = function (sequelize, DataTypes) {
         return wishlistItem;
 
     }
-
+//try to read file
         // Try to read from the file system, but move on if there"s an issue.
